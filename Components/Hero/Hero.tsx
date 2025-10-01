4792f5ccde49fc626182a2f87b7f535316fc9aca
kabil <kabilfouzai'@gmail.com>
2025-10-01 04:44:04 +0200

    test

--- Components/Hero/Hero.tsx
+++ Components/Hero/Hero.tsx
@@ -25,10 +25,10 @@ const hand = Just_Another_Hand({
 
 export default function HeroSlider() {
   const slides = [
-    { id: 1, img: "/images/slide2.jpg" },
+    { id: 1, img: "/images/Slide2.jpg" },
     { id: 2, img: "/images/Slide.jpg" },
   
-    { id: 3, img: "/images/slide4.jpg" },
+    { id: 3, img: "/images/Slide4.jpg" },
   ];
 
   return (
