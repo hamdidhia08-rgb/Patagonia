673a69d9e19719ba996ee016c93b16e53d6b6cb6
hamdidhia08-rgb <hamdidhia08@gmail.com>
2025-12-02 01:39:33 +0100

    checkout page

--- Components/checkout/ContactDetailsForm.tsx
+++ Components/checkout/ContactDetailsForm.tsx
@@ -39,15 +39,16 @@ const ContactDetailsForm: React.FC<ContactDetailsFormProps> = () => {
             Country / Region <span className="text-red-600">*</span>
           </label>
           <div className="relative">
-            <select
-              id="country"
-              name="country"
-              value="US"
-              className="block w-full appearance-none rounded-md border border-gray-300 py-3 px-3 text-gray-700 shadow-sm focus:border-red-500 focus:ring-red-500 sm:text-sm pr-10"
-              readOnly
-            >
-              <option value="US">Turkey (TK)</option>
-            </select>
+           <select
+            id="country"
+            name="country"
+            value="US"
+            className="block w-full appearance-none rounded-md border border-gray-300 py-3 px-3 text-gray-700 shadow-sm focus:border-red-500 focus:ring-red-500 sm:text-sm pr-10"
+            disabled
+          >
+            <option value="US">Turkey (TK)</option>
+          </select>
+
             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
               <svg className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 111.06 1.06l-4.25 4.25a.75.75 0 01-1.06 0l-4.25-4.25a.75.75 0 01.02-1.06z" clipRule="evenodd" />
